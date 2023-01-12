a71e50524b1f6cef0b1374767c85119d9ae63335
Gerardo Cordero <cordero.gerard@gmail.com>
2023-01-12 16:34:17 -0400

    Add Menu Navbar Component

--- web/src/app.js
+++ web/src/app.js
@@ -1,12 +1,12 @@
+import NavBar from "./components/NavBar";
 import "./index.css";
+import Login from "./pages/Login";
 
 export function App() {
   return (
     <>
-      <h1 className="text-3xl text-blue-500">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline text-pink-700">
-        Hello World!
-      </h1>
+      <NavBar></NavBar>
+      <Login></Login>
     </>
   );
 }
---
+++ web/src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import Logo from "./../images/Logo";
+
+export default function Navbar({ fixed }) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-white border-b-2 mb-3 shadow">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-black"
+              href="#pablo"
+            >
+              <Logo></Logo>
+            </a>
+            <button
+              className="text-black cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-500 hover:opacity-75"
+                  href="#pablo"
+                >
+                  <span className="ml-2">Search</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
